8bf7a1d7ebc5e9260daea646d68f8f3ef0a7ac5f
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-31 21:41:50 +0200

    Testing rendering proper info about conversion PLN -> USD

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,7 +1,27 @@
-import { render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import ResultBox from './ResultBox';
-  describe('Component ResultBox', () => {
-      it('should render without crashing', () => {
-          render(<ResultBox from="PLN" to="USD" amount={100} />);
-      });
+import '@testing-library/jest-dom/extend-expect';
+
+
+describe('Component ResultBox', () => {
+    it('should render without crashing', () => {
+        render(<ResultBox from="PLN" to="USD" amount={100} />);
+    });
+
+    it('should render proper info about conversion when PLN -> USD', () => {
+
+        const testCases = [
+            {amount: 100, result: 'PLN 100.00 = $28.57'},
+            {amount: 189, result: 'PLN 189.00 = $54.00'},
+            {amount: 73.50, result: 'PLN 73.50 = $21.00'},
+            {amount: 340.73, result: 'PLN 340.73 = $97.35'},
+        ];
+
+        for (const testCase of testCases) {
+            render(<ResultBox from="PLN" to="USD" amount={testCase.amount} />);
+            const resultField = screen.getByTestId('result');
+            expect(resultField).toHaveTextContent(testCase.result);
+            cleanup();
+        }
+    })
 });
