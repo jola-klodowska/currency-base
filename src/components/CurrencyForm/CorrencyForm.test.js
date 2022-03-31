20df1a0aca05f0fc21409a63af93289e70511f30
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-31 19:13:22 +0200

    Create loop for of to testing function in component CurrencyForm

--- src/components/CurrencyForm/CorrencyForm.test.js
+++ src/components/CurrencyForm/CorrencyForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
 import userEvent from '@testing-library/user-event';
 
@@ -8,19 +8,43 @@ describe('Component CurrencyForm', () => {
     });
 
     it('should run action callback with proper data on form submit', () => {
-        const action = jest.fn();
-    
-        // render component
-        render(<CurrencyForm action={action} />);
-    
-        // find “convert” button
-        const submitButton = screen.getByText('Convert');
-    
-        // simulate user click on "convert" button
-        userEvent.click(submitButton);
-    
-        // check if action callback was called once
-        expect(action).toHaveBeenCalledTimes(1);
-    
-      });
+
+        const testCases = [
+            { amount: 100, from: 'PLN', to: 'USD' },
+            { amount: 20, from: 'USD', to: 'PLN' },
+            { amount: 200, from: 'PLN', to: 'USD' },
+            { amount: 345, from: 'USD', to: 'PLN' },
+        ];
+
+        for (const testCase of testCases) {
+
+            const action = jest.fn();
+
+            // render component
+            render(<CurrencyForm action={action} />);
+
+            // find “convert” button
+            const submitButton = screen.getByText('Convert');
+
+            // find field elems
+            const amountField = screen.getByTestId('amount');
+            const fromField = screen.getByTestId('from-select');
+            const toField = screen.getByTestId('to-select');
+
+            // set test values to fields
+            userEvent.type(amountField, testCase.amount.toString());
+             userEvent.selectOptions(fromField, testCase.from);
+            userEvent.selectOptions(toField, testCase.to);
+
+
+            // simulate user click on "convert" button
+            userEvent.click(submitButton);
+
+            // check if action callback was called once
+            expect(action).toHaveBeenCalledTimes(1);
+            expect(action).toHaveBeenCalledWith(testCase);
+
+            cleanup();
+        }
+    });
 });
